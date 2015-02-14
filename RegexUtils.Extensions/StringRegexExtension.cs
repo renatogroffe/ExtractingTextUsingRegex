6854980da42e6103e733f079c06bef565fde7001
Renato Groffe <renato.groffe@yahoo.com.br>
2015-02-13 22:31:09 -0200

    Extraindo sequências de texto de uma string a partir de regular expressions
    Extension method para criado para uso com instâncias da classe String,
    tendo por objetivo permitir a extração de sequências de texto que
    coincidam com uma expressão regular.

---
+++ RegexUtils.Extensions/StringRegexExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RegexUtils.Extensions
+{
+    /// <summary>
+    /// Extensão que permite a extração de sequências de texto
+    /// de uma string, a partir do uso de regular expressions
+    /// </summary>
+    public static class StringRegexExtension
+    {
+        /// <summary>
+        /// Retorna todas as sequências de texto encontradas em
+        /// uma string e que atendam a uma expressão regular fornecida
+        /// como parâmetro.
+        /// </summary>
+        /// <param name="value">Valor a ser analisado.</param>
+        /// <param name="regularExpression">Expressão regular.</param>
+        /// <returns>Array com as sequências de texto encontradas.</returns>
+        public static string[] ExtractStringsUsingRegex(
+            this string value,
+            string regularExpression)
+        {
+            MatchCollection mc =
+                Regex.Matches(value, regularExpression);
+
+            return (from Match m in mc
+                    select m.Value).ToArray();
+        }
+    }
+}
---
+++ StringRegexExtension.Tests/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RegexUtils.Extensions;
+
+namespace StringRegexExtension.Tests
+{
+    class Program
+    {
+        private static void ShowResults(
+            string text, string[] results)
+        {
+            Console.WriteLine("Conteúdo da variável text = {0}", text);
+
+            StringBuilder strb = new StringBuilder();
+            strb.Append("[ ");
+            foreach (string result in results)
+            {
+                if (strb.Length > 2)
+                    strb.Append(" , ");
+                strb.Append(result);
+            }
+            strb.Append(" ]");
+
+            Console.WriteLine(
+                "Strings encontradas = {0}",
+                strb.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            string text;
+            string[] results;
+
+            text = "x=3.452 y=4521 z= 3412";
+            results = text.ExtractStringsUsingRegex(@"\d+(?:\.\d+)?");
+            ShowResults(text, results);
+
+            Console.WriteLine(String.Empty);
+
+            text = "Para entrar em contato envie um e-mail para test@acme.com. Caso não " +
+                "obtenha uma rápida resposta, encaminhe um e-mail para answers@acme.com.br.";
+            results = text.ExtractStringsUsingRegex("[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})");
+            ShowResults(text, results);
+
+            Console.ReadKey();
+        }
+    }
+}
